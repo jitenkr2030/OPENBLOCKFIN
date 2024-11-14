1e7c537252cad5c2b6bd6e5a100a42c1e15fd5d1
jitenderkr2030 <jitenderkr2030@gmail.com>
2024-11-14 15:41:44 +0530

    code added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-// Sample content 
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';  // Import global styles
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
